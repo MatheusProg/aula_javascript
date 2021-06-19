5889953394d1f007bb707278df126bed583a2d64
Matheus Dias <38333767+MatheusProg@users.noreply.github.com>
2021-06-19 11:30:06 -0300

    Introdução ao JavaScript

---
+++ script/main.js
@@ -0,0 +1,10 @@
+var nome = "Matheus Dias"
+var n1 = 23
+var n2 = 10
+var frase = "Matheus é Incrível"
+// alert(nome + " tem " + idade + " anos")
+//alert(idade + idade2)
+console.log(nome)
+console.log(n1 * n2)
+console.log(frase.replace("Incrível", "Super incrível"))
+//alert(frase.replace("Incrível", "Super incrível"))
