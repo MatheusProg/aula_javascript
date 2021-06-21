36d32790e76e76ed6eeb7968e322af4fcd92f08a
Matheus Dias <38333767+MatheusProg@users.noreply.github.com>
2021-06-21 12:59:20 -0300

    Array e Dicionário

--- script/main.js
+++ script/main.js
@@ -1,10 +1,31 @@
-var nome = "Matheus Dias"
-var n1 = 23
-var n2 = 10
-var frase = "Matheus é Incrível"
+
+
+var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas)
+alert(frutas[1].nome)
+
+/*
+var fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta.nome)
+alert(fruta.cor)
+*/
+
+//var lista = ["maça", "pêra", "laranja"]
+//lista.push("uva")
+//lista.pop()
+
+//console.log(lista)
+//console.log(lista.toString())
+//console.log(lista.join(" - "))
+
+
+//var nome = "Matheus Dias"
+//var n1 = 23
+//var n2 = 10
+//var frase = "Matheus é Incrível"
 // alert(nome + " tem " + idade + " anos")
 //alert(idade + idade2)
-console.log(nome)
-console.log(n1 * n2)
-console.log(frase.replace("Incrível", "Super incrível"))
+//console.log(nome)
+//console.log(n1 * n2)
+//console.log(frase.replace("Incrível", "Super incrível"))
 //alert(frase.replace("Incrível", "Super incrível"))
